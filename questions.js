5dfdf279771df6913e48cbdd5a9159ad89d87304
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-24 18:50:22 +0700

    Update questions 0.0.3.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- questions.js
+++ questions.js
@@ -3,61 +3,62 @@ const questions = [
         question: "这是什么?",
         answers: ["电脑 (diànnǎo)", "电话 (diànhuà)", "电灯 (diàndēng)", "电影 (diànyǐng)"],
         correct: "电脑 (diànnǎo)",
-        image: "images/hanoi.jpg" // Thêm trường image với đường dẫn đến ảnh
+        image: "images/maytinh.jpg" // Thêm trường image với đường dẫn đến ảnh
     },
     {
         question: "这是什么?",
         answers: ["纸巾 (zhǐjīn)", "报纸 (bàozhǐ)", "面巾 (miànjīn)", "毛巾 (máojīn)"],
         correct: "纸巾 (zhǐjīn)",
-        image: "images/biology.jpg" // Thêm trường image
+        image: "images/khangiay.jpg" // Thêm trường image
     },
     {
         question: "这是什么？",
         answers: ["手表 (shǒubiǎo)", "手机 (shǒujī)", "手套 (shǒutào)", "手链 (shǒuliàn)"],
         correct: "手表 (shǒubiǎo)",
-        image: "images/sunrise.jpg" // Thêm trường image
+        image: "images/dongho.jpg" // Thêm trường image
     },
     {
         question: "这是什么?",
         answers: ["牙刷 (yáshuā)", "牙膏 (yágāo)", "毛巾 (máojīn)", "梳子 (shūzi)"],
         correct: "牙刷 (yáshuā)",
-        image: "images/giza.jpg" // Thêm trường image
+        image: "images/banchaidanhrang.jpg" // Thêm trường image
     },
     {
         question: "这是什么?",
         answers: ["手机 (shǒujī)", "手表 (shǒubiǎo)", "电话 (diànhuà)", "电脑 (diànnǎo)"],
         correct: "手机 (shǒujī)",
-        image: "images/tiger.jpg"
+        image: "images/dienthoai.jpg"
     },
     {
         question: "这是什么?",
-        answers: ["占婆", "高棉", "京族", "苗族"],
-        correct: "占婆",
-        image: "images/india.jpg"
+        answers: ["柜子 (guìzi)", "箱子 (xiāngzi)", "盒子 (hézi)", "橱子 (chúzi)"],
+        correct: "柜子 (guìzi)",
+        image: "images/caitu.jpg"
     },
     {
         question: "这是什么?",
         answers: ["牙膏 (yágāo)", "牙刷 (yáshuā)", "香皂 (xiāngzào)", "洗发水 (xǐfàshuǐ)"],
         correct: "牙膏 (yágāo)",
-        image: "images/kidney.jpg"
+        image: "images/kemdanhrang.jpg"
     },
     {
         question: "这是什么?",
         answers: ["吹风机 (chuīfēngjī)", "洗衣机 (xǐyījī)", "空调 (kōngtiáo)", "电风扇 (diànfēngshàn)"],
         correct: "吹风机 (chuīfēngjī)",
-        image: "images/oxygen.jpg"
+        image: "images/maysaytoc.jpg"
     },
     {
         question: "这是什么?",
         answers: ["衣服 (yīfu)", "裤子 (kùzi)", "裙子 (qúnzi)", "外套 (wàitào)"],
         correct: "衣服 (yīfu)",
-        image: "images/edison.jpg"
+        image: "images/quanao.jpg"
     },
     {
         question: "这是什么?",
         answers: ["桌子 (zhuōzi)", "椅子 (yǐzi)", "凳子 (dèngzi)", "床 (chuáng)"],
         correct: "桌子 (zhuōzi)",
-        image: "images/mercury.jpg"
+        image: "images/caiban.jpg"
     }
 
 ];
+
