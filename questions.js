8a831ffae34fbe1f74489c846255964c29602617
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-24 09:08:08 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+const questions = [
+    {
+        question: "越南的首都叫什么名字?",
+        answers: ["胡志明市", "河内", "岘港", "海防"],
+        correct: "河内",
+        image: "images/hanoi.jpg" // Thêm trường image với đường dẫn đến ảnh
+    },
+    {
+        question: "越南著名的下龙湾位于哪个省?",
+        answers: ["广宁省", "河内", "顺化市", "胡志明市"],
+        correct: "广宁省",
+        image: "images/biology.jpg" // Thêm trường image
+    },
+    {
+        question: "古代越南的国号“大越”始于哪个朝代？",
+        answers: ["陈朝", "黎朝", "李朝", "阮朝"],
+        correct: "李朝",
+        image: "images/sunrise.jpg" // Thêm trường image
+    },
+    {
+        question: "越南的古都顺化位于哪条河的两岸?",
+        answers: ["红河", "湄公河", "香江", "北江"],
+        correct: "香江",
+        image: "images/giza.jpg" // Thêm trường image
+    },
+    {
+        question: "奠边府战役发生在哪一年?",
+        answers: ["1945 年", "1954 年", "1968 年", "1975 年"],
+        correct: "1954 年",
+        image: "images/tiger.jpg"
+    },
+    {
+        question: "越南著名的美山圣地是哪个民族的古代遗址?",
+        answers: ["占婆", "高棉", "京族", "苗族"],
+        correct: "占婆",
+        image: "images/india.jpg"
+    },
+    {
+        question: "越南的会安古城被列入世界文化遗产是哪一年?",
+        answers: ["1993 年", "1995 年", "1999 年", "2003 年"],
+        correct: "1999 年",
+        image: "images/kidney.jpg"
+    },
+    {
+        question: "在越南历史上,哪位将军在1954年被誉为抗法战争的民族英雄?",
+        answers: ["胡志明", "武元甲", "黎利", "陈兴道"],
+        correct: "武元甲",
+        image: "images/oxygen.jpg"
+    },
+    {
+        question: "下列哪一个是越南最长的河流?",
+        answers: ["红河", "湄公河", "香江", "北江"],
+        correct: "湄公河",
+        image: "images/edison.jpg"
+    },
+    {
+        question: "越南的独立日是在哪一天?",
+        answers: ["9 月 2 日", "4 月 30 日", "8 月 19 日", "11 月 25 日"],
+        correct: "9 月 2 日",
+        image: "images/mercury.jpg"
+    }
+];
