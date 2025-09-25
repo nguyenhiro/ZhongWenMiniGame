8c06f9b2a32aa3255b6fdc00a93cb209317dbd61
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-26 00:17:25 +0700

    Update questions 0.0.9.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- questions.js
+++ questions.js
@@ -103,8 +103,124 @@ const questions = [
         question: "Hoài bão, ước mơ",
         answers: ["抱负 (bàofù)", "报复 (bàofù)"],
         correct: "抱负 (bàofù)",
+    },
+            {
+        question: "Bệnh viện",
+        answers: ["医院 (yīyuàn)", "一院 (yīyuàn)"],
+        correct: "医院 (yīyuàn)",
+    },
+            {
+        question: "Mặt trời",
+        answers: ["太阳 (tàiyáng)", "太痒 (tàiyǎng)"],
+        correct: "太阳 (tàiyáng)",
+    },
+            {
+        question: "Ban ngày",
+        answers: ["白天 (báitiān)", "明白 (míng bái)"],
+        correct: "白天 (báitiān)",
+    },
+            {
+        question: "Nhà vệ sinh",
+        answers: ["洗手间 (xǐshǒujiān)", "洗手 (xǐ shǒu)"],
+        correct: "洗手间 (xǐshǒujiān)",
+    },
+            {
+        question: "Áo khoác",
+        answers: ["外套 (wàitào)", "衣服 (yīfu)"],
+        correct: "外套 (wàitào)",
+    },
+            {
+        question: "Cái đinh",
+        answers: ["钉子 (dīngzi)", "锤子 (chuízi)"],
+        correct: "钉子 (dīngzi)",
+    },
+            {
+        question: "Gội đầu",
+        answers: ["洗发 (xǐfà)", "洗脸 (xǐ liǎn)"],
+        correct: "洗发 (xǐfà)",
+    },
+            {
+        question: "Rau củ",
+        answers: ["蔬菜 (shūcài)", "炒菜 (chǎo cài)"],
+        correct: "蔬菜 (shūcài)",
+    },
+            {
+        question: "Bia",
+        answers: ["酒 (jiǔ)", "啤酒 (píjiǔ)"],
+        correct: "啤酒 (píjiǔ)",
+    },
+            {
+        question: "Hài lòng",
+        answers: ["满溢 (mǎnyì)", "满意 (mǎnyì)"],
+        correct: "满意 (mǎnyì)",
+    },
+            {
+        question: "Văn phòng",
+        answers: ["办公室 (bàngōngshì)", "公司 (gōngsī)"],
+        correct: "办公室 (bàngōngshì)",
+    },
+            {
+        question: "Báo cáo",
+        answers: ["报告 (bàogào)", "包子 (bāozi)"],
+        correct: "报告 (bàogào)",
+    },
+            {
+        question: "Sổ tay, máy tính xách tay (laptop)",
+        answers: ["笔记本 (bǐjìběn)", "圆珠笔 (yuánzhūbǐ)"],
+        correct: "笔记本 (bǐjìběn)",
+    },
+            {
+        question: "Báo cáo tuần",
+        answers: ["周报 (zhōubào)", "周末 (zhōumò)"],
+        correct: "周报 (zhōubào)",
+    },
+            {
+        question: "Thăm bạn bè",
+        answers: ["看朋友 (kàn péngyǒu)", "看报 (kànbào)"],
+        correct: "看朋友 (kàn péngyǒu)",
+    },
+            {
+        question: "Đi làm",
+        answers: ["上班 (shàngbān)", "上课 (shàngkè)"],
+        correct: "上班 (shàngbān)",
+    },
+            {
+        question: "Tan học",
+        answers: ["下班 (xiàbān)", "下课 (xiàkè)"],
+        correct: "下课 (xiàkè)",
+    },
+            {
+        question: "Ông chủ",
+        answers: ["老板 (lǎobǎn)", "老伴 (lǎobàn)"],
+        correct: "老板 (lǎobǎn)",
+    },
+            {
+        question: "Tài liệu",
+        answers: ["文件 (wénjiàn)", "课文 (kèwén)"],
+        correct: "文件 (wénjiàn)",
+    },
+                {
+        question: "Giá cả",
+        answers: ["价格 (jiàgé)", "讨价还价 (tǎojià huánjià)"],
+        correct: "价格 (jiàgé)",
+    },
+                {
+        question: "Đồng nghiệp",
+        answers: ["同事 (tóngshì)", "同桌 (tóngzhuō)"],
+        correct: "同事 (tóngshì)",
+    },
+                {
+        question: "Khách hàng",
+        answers: ["客户 (kèhù)", "客气 (kèqi)"],
+        correct: "客户 (kèhù)",
+    },
+                {
+        question: "Đầu tư",
+        answers: ["投资 (tóuzī): Đầu tư", "亏损 (kuīsǔn)"],
+        correct: "投资 (tóuzī): Đầu tư",
     }
 ];
+
 
 
 
