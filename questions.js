d9c6f9f7bba87c4c118f02fc099507856b7c580e
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-25 23:22:52 +0700

    Update questions 0.0.8.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- questions.js
+++ questions.js
@@ -48,9 +48,64 @@ const questions = [
         question: "Cái bàn",
         answers: ["桌子 (zhuōzi)", "椅子 (yǐzi)"],
         correct: "桌子 (zhuōzi)",
+    },
+    {
+        question: "Được không?",
+        answers: ["行不行?", "好不好?"],
+        correct: "行不行?",
+    },
+        {
+        question: "Gà trống",
+        answers: ["攻击 (gōngjī)", "公鸡 (gōngjī)"],
+        correct: "公鸡 (gōngjī)",
+    },
+        {
+        question: "Ký ức, trí nhớ",
+        answers: ["记忆 (jìyì)", "技艺 (jìyì)"],
+        correct: "记忆 (jìyì)",
+    },
+        {
+        question: "Ý tưởng, ý kiến",
+        answers: ["注意 (zhùyì)", "主意 (zhǔyi)"],
+        correct: "主意 (zhǔyi)",
+    },
+        {
+        question: "Lợi hại",
+        answers: ["厉害 (lìhai)", "利害 (lìhài)"],
+        correct: "厉害 (lìhai)",
+    },
+        {
+        question: "Mục đích",
+        answers: ["目的 (mùdì)", "墓地 (mùdì)"],
+        correct: "目的 (mùdì)",
+    },
+        {
+        question: "Đồ trang sức",
+        answers: ["首饰 (shǒushì)", "手势 (shǒushì)"],
+        correct: "首饰 (shǒushì)",
+    },
+        {
+        question: "Cuộc họp",
+        answers: ["会议 (huìyì)", "回忆 (huíyì)"],
+        correct: "会议 (huìyì)",
+    },
+        {
+        question: "Giám đốc",
+        answers: ["经历 (jīnglì)", "经理 (jīnglǐ)"],
+        correct: "经理 (jīnglǐ)",
+    },
+        {
+        question: "Luyện tập",
+        answers: ["练习 (liànxí)", "联系 (liánxì)"],
+        correct: "练习 (liànxí)",
+    },
+        {
+        question: "Hoài bão, ước mơ",
+        answers: ["抱负 (bàofù)", "报复 (bàofù)"],
+        correct: "抱负 (bàofù)",
     }
-
 ];
+
 
 
 
