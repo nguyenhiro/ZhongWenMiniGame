a4adaf1ba5d3ef0399bf8fb980fe532db86129a2
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-28 19:43:50 +0700

    Add files via upload
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

---
+++ levels/level1.js
@@ -0,0 +1,14 @@
+const level1Questions = [
+  {
+    question: "Máy tính",
+    answers: ["电脑 (diànnǎo)", "电话 (diànhuà)"],
+    correct: "电脑 (diànnǎo)",
+  },
+  {
+    question: "khăn giấy",
+    answers: ["纸巾 (zhǐjīn)", "报纸 (bàozhǐ)"],
+    correct: "纸巾 (zhǐjīn)",
+  },
+];
+
+export default level1Questions;
