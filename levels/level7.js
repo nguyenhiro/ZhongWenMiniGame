455910d327174e4f5a72bc331b54d2b11c9f63cd
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 11:53:04 +0700

    Add files via upload
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

---
+++ levels/level7.js
@@ -0,0 +1,14 @@
+const level7Questions = [
+  {
+    question: "Câu hỏi 1 cấp độ 2",
+    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
+    correct: "Đáp án A",
+  },
+  {
+    question: "Câu hỏi 2 cấp độ 2",
+    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
+    correct: "Đáp án Y",
+  },
+];
+
+export default level7Questions;
