758ae6fc1ef20075019a4b1d767e2ab50a30247f
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:44:16 +0700

    Update level3.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level3.js
+++ levels/level3.js
@@ -1,14 +1,84 @@
 const level3Questions = [
   {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+        question: "Ngân hàng",
+        answers: ["银行 (yínháng)", "取 (qǔ)"],
+        correct: "银行 (yínháng)",
+    },
+    {
+        question: "Tiền",
+        answers: ["钱 (qián)", "七 (qī)"],
+        correct: "钱 (qián)",
+    },
+    {
+        question: "Chín",
+        answers: ["六 (liù)", "九 (jiǔ)"],
+        correct: "九 (jiǔ)",
+    },
+    {
+        question: "Tên riêng",
+        answers: ["北京 (Běijīng)", "专名 (zhuānmíng)"],
+        correct: "专名 (zhuānmíng)",
+    },
+    {
+        question: "Ngày, trời",
+        answers: ["今天 (jīntiān)", "天 (tiān)"],
+        correct: "天 (tiān)",
+    },
+    {
+        question: "Ngày kia",
+        answers: ["后天 (hòutiān)", "大后天 (dàhòutiān)"],
+        correct: "大后天 (dàhòutiān)",
+    },
+    {
+        question: "Tuần, thứ",
+        answers: ["星期 (xīngqī)", "周末 (zhōumò)"],
+        correct: "星期 (xīngqī)",
+    },
+    {
+        question: "Thứ 6",
+        answers: ["星期一 (xīngqī yī)", "星期五 (xīngqī wǔ)"],
+        correct: "星期五 (xīngqī wǔ)",
+    },
+    {
+        question: "Thứ 3",
+        answers: ["星期二 (xīngqī èr)", "星期三 (xīngqī sān)"],
+        correct: "星期二 (xīngqī èr)",
+    },
+    {
+        question: "Thứ 7",
+        answers: ["星期四 (xīngqī sì)", "星期六 (xīngqī liù)"],
+        correct: "星期六 (xīngqī liù)",
+    },
+    {
+        question: "Chủ nhật",
+        answers: ["星期天 (xīngqī tiān)", "周末 (zhōumò)"],
+        correct: "星期天 (xīngqī tiān)",
+    },
+    {
+        question: "Cuối tuần",
+        answers: ["周末 (zhōumò)", "星期日 (xīngqī rì)"],
+        correct: "周末 (zhōumò)",
+    },
+    {
+        question: "Hai",
+        answers: ["几 (jǐ)", "二 (èr)"],
+        correct: "二 (èr)",
+    },
+    {
+        question: "Bốn",
+        answers: ["三 (sān)", "四 (sì)"],
+        correct: "四 (sì)",
+    },
+    {
+        question: "Ở đâu",
+        answers: ["哪儿 (nǎr)", "那儿 (nàr)"],
+        correct: "哪儿 (nǎr)",
+    },
+    {
+        question: "Biết, sẽ, có thể",
+        answers: ["会 (huì)", "回 (huí)"],
+        correct: "会 (huì)",
+    },
 ];
 
 export default level3Questions;
