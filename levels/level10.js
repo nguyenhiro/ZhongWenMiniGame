766bb7c0dfcb404444a156feb4f35de2ef295f08
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:47:11 +0700

    Update level10.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level10.js
+++ levels/level10.js
@@ -1,14 +1,84 @@
 const level10Questions = [
   {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+        question: "Điền Phương",
+        answers: ["马 (mǎ)", "田芳 (Tián Fāng)"],
+        correct: "田芳 (Tián Fāng)",
+    },
+    {
+        question: "Ái Đức Hoa, Edward",
+        answers: ["爱德华 (Àidéhuá)", "罗兰 (Luólán)"],
+        correct: "爱德华 (Àidéhuá)",
+    },
+    {
+        question: "Ngôn ngữ",
+        answers: ["语言 (yǔyán)", "大学 (dàxué)"],
+        correct: "语言 (yǔyán)",
+    },
+    {
+        question: "Thế nào, ra sao, như thế nào",
+        answers: ["怎么样 (zěnmeyàng)", "觉得 (juéde)"],
+        correct: "怎么样 (zěnmeyàng)",
+    },
+    {
+        question: "Cảm thấy",
+        answers: ["觉得 (juéde)", "语法 (yǔfǎ)"],
+        correct: "觉得 (juéde)",
+    },
+    {
+        question: "Nghe",
+        answers: ["听 (tīng)", "和 (hé)"],
+        correct: "听 (tīng)",
+    },
+    {
+        question: "Nói",
+        answers: ["说 (shuō)", "比较 (bǐjiào)"],
+        correct: "说 (shuō)",
+    },
+    {
+        question: "Tương đối",
+        answers: ["比较 (bǐjiào)", "容易 (róngyì)"],
+        correct: "比较 (bǐjiào)",
+    },
+    {
+        question: "Dễ",
+        answers: ["容易 (róngyì)", "读 (dú)"],
+        correct: "容易 (róngyì)",
+    },
+    {
+        question: "Đọc",
+        answers: ["读 (dú)", "写 (xiě)"],
+        correct: "读 (dú)",
+    },
+    {
+        question: "Nhưng",
+        answers: ["但是 (dànshì)", "给 (gěi)"],
+        correct: "但是 (dànshì)",
+    },
+    {
+        question: "Mới",
+        answers: ["新 (xīn)", "北京语言大学 (Běijīng Yǔyán Dàxué)"],
+        correct: "新 (xīn)",
+    },
+    {
+        question: "Bạn cùng phòng",
+        answers: ["同学 (tóngxué)", "同屋 (tóngwū)"],
+        correct: "同屋 (tóngwū)",
+    },
+    {
+        question: "Lâm",
+        answers: ["班 (bān)", "林 (Lín)"],
+        correct: "林 (Lín)",
+    },
+    {
+        question: "Vali, thùng, hộp",
+        answers: ["没有 (méiyǒu)", "箱子 (xiāngzi)"],
+        correct: "箱子 (xiāngzi)",
+    },
+    {
+        question: "Ở đây",
+        answers: ["有 (yǒu)", "这儿 (zhèr)"],
+        correct: "这儿 (zhèr)",
+    },
 ];
 
 export default level10Questions;
