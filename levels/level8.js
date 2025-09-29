8ff6e705f5dcd23cd62986cc97f536e2ff91928e
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:46:23 +0700

    Update level8.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level8.js
+++ levels/level8.js
@@ -1,14 +1,84 @@
 const level8Questions = [
   {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+        question: "Quýt",
+        answers: ["橘子 (júzi)", "怎么 (zěnme)"],
+        correct: "橘子 (júzi)",
+    },
+    {
+        question: "Tổng cộng",
+        answers: ["一共 (yígòng)", "两 (liǎng)"],
+        correct: "一共 (yígòng)",
+    },
+    {
+        question: "Đưa, cho",
+        answers: ["给 (gěi)", "找 (zhǎo)"],
+        correct: "给 (gěi)",
+    },
+    {
+        question: "Buổi chiều",
+        answers: ["下午 (xiàwǔ)", "上午 (shàngwǔ)"],
+        correct: "下午 (xiàwǔ)",
+    },
+    {
+        question: "Thư viện",
+        answers: ["图书馆 (túshūguǎn)", "营业员 (yíngyèyuán)"],
+        correct: "图书馆 (túshūguǎn)",
+    },
+    {
+        question: "Đổi, trao đổi",
+        answers: ["换 (huàn)", "小姐 (xiǎojiě)"],
+        correct: "换 (huàn)",
+    },
+    {
+        question: "Nhân dân tệ",
+        answers: ["人民币 (Rénmínbì)", "人民 (rénmín)"],
+        correct: "人民币 (Rénmínbì)",
+    },
+    {
+        question: "Nghìn",
+        answers: ["百 (bǎi)", "千 (qiān)"],
+        correct: "千 (qiān)",
+    },
+    {
+        question: "Vạn",
+        answers: ["万 (wàn)", "百 (bǎi)"],
+        correct: "万 (wàn)",
+    },
+    {
+        question: "Đô la mỹ, USD",
+        answers: ["美元 (Měiyuán)", "港币 (Gǎngbì)"],
+        correct: "美元 (Měiyuán)",
+    },
+    {
+        question: "EURO",
+        answers: ["日元 (Rìyuán)", "欧元 (Ōuyuán)"],
+        correct: "欧元 (Ōuyuán)",
+    },
+    {
+        question: "Chờ, đợi/ vv...",
+        answers: ["等 (děng)", "一会儿 (yíhuìr)"],
+        correct: "等 (děng)",
+    },
+    {
+        question: "Tiên shinh",
+        answers: ["先生 (xiānsheng)", "数 (shù / shǔ)"],
+        correct: "先生 (xiānsheng)",
+    },
+    {
+        question: "Làm việc văn phòng",
+        answers: ["办公室 (bàngōngshì)", "办公 (bàngōng)"],
+        correct: "办公 (bàngōng)",
+    },
+    {
+        question: "Nhân viên",
+        answers: ["职员 (zhíyuán)", "找 (zhǎo)"],
+        correct: "职员 (zhíyuán)",
+    },
+    {
+        question: "Tìm, trả lại tiền thừa",
+        answers: ["找 (zhǎo)", "在 (zài)"],
+        correct: "找 (zhǎo)",
+    },
 ];
 
 export default level8Questions;
