06a56d76cc5b0e73b62a3d50cf503d5dc5c0bcb3
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:46:46 +0700

    Update level9.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level9.js
+++ levels/level9.js
@@ -1,14 +1,84 @@
 const level9Questions = [
-  {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+ {
+        question: "Nhà, gia đình",
+        answers: ["家 (jiā)", "呢 (ne)"],
+        correct: "家 (jiā)",
+    },
+    {
+        question: "Ở, cư trú",
+        answers: ["住 (zhù)", "楼 (lóu)"],
+        correct: "住 (zhù)",
+    },
+    {
+        question: "Căn phòng",
+        answers: ["门 (mén)", "房间 (fángjiān)"],
+        correct: "房间 (fángjiān)",
+    },
+    {
+        question: "Biết",
+        answers: ["号 (hào)", "知道 (zhīdào)"],
+        correct: "知道 (zhīdào)",
+    },
+    {
+        question: "Điện thoại bàn",
+        answers: ["电话 (diànhuà)", "电 (diàn)"],
+        correct: "电话 (diànhuà)",
+    },
+    {
+        question: "Mã số",
+        answers: ["话 (huà)", "号码 (hàomǎ)"],
+        correct: "号码 (hàomǎ)",
+    },
+    {
+        question: "Số 0, lẻ",
+        answers: ["零 (líng)", "手 (shǒu)"],
+        correct: "零 (líng)",
+    },
+    {
+        question: "Lý Sáng Hạo",
+        answers: ["李昌浩 (Lǐ Chānghào)", "秘书 (mìshū)"],
+        correct: "李昌浩 (Lǐ Chānghào)",
+    },
+    {
+        question: "Giới thiệu",
+        answers: ["先 (xiān)", "介绍 (jièshào)"],
+        correct: "介绍 (jièshào)",
+    },
+    {
+        question: "Vị",
+        answers: ["一下儿 (yíxiàr)", "位 (wèi)"],
+        correct: "位 (wèi)",
+    },
+    {
+        question: "Hoan nghênh, chào mừng",
+        answers: ["欢迎 (huānyíng)", "留学生 (liúxuéshēng)"],
+        correct: "欢迎 (huānyíng)",
+    },
+    {
+        question: "Cũng",
+        answers: ["留学 (liúxué)", "也 (yě)"],
+        correct: "也 (yě)",
+    },
+    {
+        question: "Chúng tôi",
+        answers: ["我们 (wǒmen)", "你们 (nǐmen)"],
+        correct: "我们 (wǒmen)",
+    },
+    {
+        question: "Họ, bọn họ (nữ)",
+        answers: ["他们 (tāmen)", "她们 (tāmen)"],
+        correct: "她们 (tāmen)",
+    },
+    {
+        question: "Lượng từ 2 người",
+        answers: ["都 (dōu)", "俩 (liǎ)"],
+        correct: "俩 (liǎ)",
+    },
+    {
+        question: "Không có gì",
+        answers: ["学生 (xuésheng)", "没什么 (méi shénme)"],
+        correct: "没什么 (méi shénme)",
+    },
 ];
 
 export default level9Questions;
