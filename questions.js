9ae6622a7d9822c14571c201234e1a69f2dd29a3
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:48:13 +0700

    Update questions.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- questions.js
+++ questions.js
@@ -8,6 +8,26 @@ import level7Questions from './levels/level7.js';
 import level8Questions from './levels/level8.js';
 import level9Questions from './levels/level9.js';
 import level10Questions from './levels/level10.js';
+import level11Questions from './levels/level11.js';
+import level12Questions from './levels/level12.js';
+import level13Questions from './levels/level13.js';
+import level14Questions from './levels/level14.js';
+import level15Questions from './levels/level15.js';
+import level16Questions from './levels/level16.js';
+import level17Questions from './levels/level17.js';
+import level18Questions from './levels/level18.js';
+import level19Questions from './levels/level19.js';
+import level20Questions from './levels/level20.js';
+import level21Questions from './levels/level21.js';
+import level22Questions from './levels/level22.js';
+import level23Questions from './levels/level23.js';
+import level24Questions from './levels/level24.js';
+import level25Questions from './levels/level25.js';
+import level26Questions from './levels/level26.js';
+import level27Questions from './levels/level27.js';
+import level28Questions from './levels/level28.js';
+import level29Questions from './levels/level29.js';
+import level30Questions from './levels/level30.js';
 
 const questionsByLevel = {
   1: level1Questions,
@@ -20,6 +40,26 @@ const questionsByLevel = {
   8: level8Questions,
   9: level9Questions,
   10: level10Questions,
+  11: level11Questions,
+  12: level12Questions,
+  13: level13Questions,
+  14: level14Questions,
+  15: level15Questions,
+  16: level16Questions,
+  17: level17Questions,
+  18: level18Questions,
+  19: level19Questions,
+  20: level20Questions,
+  21: level21Questions,
+  22: level22Questions,
+  23: level23Questions,
+  24: level24Questions,
+  25: level25Questions,
+  26: level26Questions,
+  27: level27Questions,
+  28: level28Questions,
+  29: level29Questions,
+  30: level30Questions,
 };
 
 // Export toàn bộ đối tượng ra ngoài
