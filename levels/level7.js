34fdabdd7211f30a9097aaf46ce8011aec4433ed
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:46:00 +0700

    Update level7.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level7.js
+++ levels/level7.js
@@ -1,14 +1,84 @@
 const level7Questions = [
-  {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+{
+        question: "Canh, súp",
+        answers: ["蛋 (dàn)", "汤 (tāng)"],
+        correct: "汤 (tāng)",
+    },
+    {
+        question: "Những cái này",
+        answers: ["这些 (zhèxiē)", "一些 (yìxiē)"],
+        correct: "这些 (zhèxiē)",
+    },
+    {
+        question: "Những cái kia",
+        answers: ["那些 (nàxiē)", "些 (xiē)"],
+        correct: "那些 (nàxiē)",
+    },
+    {
+        question: "Há cảo, sủi cảo",
+        answers: ["饺子 (jiǎozi)", "包子 (bāozi)"],
+        correct: "饺子 (jiǎozi)",
+    },
+    {
+        question: "Mì, bún, sợi mì",
+        answers: ["面条 (miàntiáo)", "玛丽 (Mǎlì)"],
+        correct: "面条 (miàntiáo)",
+    },
+    {
+        question: "Bán",
+        answers: ["买 (mǎi)", "卖 (mài)"],
+        correct: "卖 (mài)",
+    },
+    {
+        question: "Kilogram",
+        answers: ["斤 (jīn)", "公斤 (gōngjīn)"],
+        correct: "公斤 (gōngjīn)",
+    },
+    {
+        question: "Táo",
+        answers: ["水果 (shuǐguǒ)", "苹果 (píngguǒ)"],
+        correct: "苹果 (píngguǒ)",
+    },
+    {
+        question: "Đắt, quý",
+        answers: ["贵 (guì)", "了 (le)"],
+        correct: "贵 (guì)",
+    },
+    {
+        question: "Bao nhiêu",
+        answers: ["吧 (ba)", "多少 (duōshǎo)"],
+        correct: "多少 (duōshǎo)",
+    },
+    {
+        question: "Nhiều, hơn",
+        answers: ["多 (duō)", "少 (shǎo)"],
+        correct: "多 (duō)",
+    },
+    {
+        question: "Đồng, tệ",
+        answers: ["块 (kuài)", "角 (jiǎo)"],
+        correct: "块 (kuài)",
+    },
+    {
+        question: "Hào",
+        answers: ["毛 (máo)", "元 (yuán)"],
+        correct: "毛 (máo)",
+    },
+    {
+        question: "Xu",
+        answers: ["分 (fēn)", "块 (kuài)"],
+        correct: "分 (fēn)",
+    },
+    {
+        question: "Vẫn, trả lại",
+        answers: ["还 (hái / huán)", "别的 (bié de)"],
+        correct: "还 (hái / huán)",
+    },
+    {
+        question: "Khác",
+        answers: ["别的 (bié de)", "橘子 (júzi)"],
+        correct: "别的 (bié de)",
+    },
 ];
 
 export default level7Questions;
