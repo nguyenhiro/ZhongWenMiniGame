e8ced42e3e916c68a474855be01e633d0b126962
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:44:40 +0700

    Update level4.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level4.js
+++ levels/level4.js
@@ -1,14 +1,84 @@
 const level4Questions = [
   {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+        question: "Lớp học, phòng học",
+        answers: ["学校 (xuéxiào)", "教室 (jiàoshì)"],
+        correct: "教室 (jiàoshì)",
+    },
+    {
+        question: "Lại đến, làm lại lần nữa",
+        answers: ["再见 (zàijiàn)", "再来 (zài lái)"],
+        correct: "再来 (zài lái)",
+    },
+    {
+        question: "Xin lỗi",
+        answers: ["对不起 (duìbuqǐ)", "不客气 (bú kèqi)"],
+        correct: "对不起 (duìbuqǐ)",
+    },
+    {
+        question: "Không sao",
+        answers: ["没关系 (méiguānxi)", "不客气 (bú kèqi)"],
+        correct: "没关系 (méiguānxi)",
+    },
+    {
+        question: "Thiên An",
+        answers: ["天安门 (Tiān’ānmén)", "天安 (Tiān’ān)"],
+        correct: "天安 (Tiān’ān)",
+    },
+    {
+        question: "Đây, này",
+        answers: ["这 (zhè)", "是 (shì)"],
+        correct: "这 (zhè)",
+    },
+    {
+        question: "Thầy giáo, cô giáo",
+        answers: ["老师 (lǎoshī)", "您 (nín)"],
+        correct: "老师 (lǎoshī)",
+    },
+    {
+        question: "Mời, xin",
+        answers: ["请 (qǐng)", "进 (jìn)"],
+        correct: "请 (qǐng)",
+    },
+    {
+        question: "Ngồi",
+        answers: ["做 (zuò)", "坐 (zuò)"],
+        correct: "坐 (zuò)",
+    },
+    {
+        question: "Uống",
+        answers: ["和 (hé)", "喝 (hē)"],
+        correct: "喝 (hē)",
+    },
+    {
+        question: "Trà",
+        answers: ["茶 (chá)", "谢谢 (xièxie)"],
+        correct: "茶 (chá)",
+    },
+    {
+        question: "Cảm tạ, cảm ơn, biết ơn",
+        answers: ["感谢 (gǎnxiè)", "客气 (kèqi)"],
+        correct: "感谢 (gǎnxiè)",
+    },
+    {
+        question: "Thân thể, sức khoẻ",
+        answers: ["多谢 (duōxiè)", "身体 (shēntǐ)"],
+        correct: "身体 (shēntǐ)",
+    },
+    {
+        question: "Việc, sự việc",
+        answers: ["十 (shí)", "事 (shì)"],
+        correct: "事 (shì)",
+    },
+    {
+        question: "Vương",
+        answers: ["日 (rì)", "王 (Wáng)"],
+        correct: "王 (Wáng)",
+    },
+    {
+        question: "Giáo sư",
+        answers: ["教授 (jiàoshòu)", "校长 (xiàozhǎng)"],
+        correct: "教授 (jiàoshòu)",
+    },
 ];
 
 export default level4Questions;
