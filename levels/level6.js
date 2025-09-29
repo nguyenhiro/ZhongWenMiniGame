7cc2afbc876f73d156c2e81ef5b878a77f93695c
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:45:25 +0700

    Update level6.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level6.js
+++ levels/level6.js
@@ -1,14 +1,84 @@
 const level6Questions = [
-  {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+ {
+        question: "Tiếng Trung",
+        answers: ["文 (wén)", "中文 (Zhōngwén)"],
+        correct: "中文 (Zhōngwén)",
+    },
+    {
+        question: "Tiếng Ả Rập",
+        answers: ["阿拉伯文 (Ālābó wén)", "德文 (Dé wén)"],
+        correct: "阿拉伯文 (Ālābó wén)",
+    },
+    {
+        question: "Tiếng Nga",
+        answers: ["俄文 (É wén)", "法文 (Fǎ wén)"],
+        correct: "俄文 (É wén)",
+    },
+    {
+        question: "Tiếng Nhật",
+        answers: ["韩文 (Hán wén)", "日文 (Rì wén)"],
+        correct: "日文 (Rì wén)",
+    },
+    {
+        question: "Tiếng Tây Ban Nha",
+        answers: ["西班牙文 (Xībānyá wén)", "英文 (Yīng wén)"],
+        correct: "西班牙文 (Xībānyá wén)",
+    },
+    {
+        question: "Mike",
+        answers: ["朋友 (péngyǒu)", "麦克 (Màikè)"],
+        correct: "麦克 (Màikè)",
+    },
+    {
+        question: "Trương Đông",
+        answers: ["张东 (Zhāng Dōng)", "麦克 (Màikè)"],
+        correct: "张东 (Zhāng Dōng)",
+    },
+    {
+        question: "Giáo sư",
+        answers: ["教授 (jiàoshòu)", "校长 (xiàozhǎng)"],
+        correct: "教授 (jiàoshòu)",
+    },
+    {
+        question: "Buổi trưa",
+        answers: ["中午 (zhōngwǔ)", "吃 (chī)"],
+        correct: "中午 (zhōngwǔ)",
+    },
+    {
+        question: "Ăn",
+        answers: ["吃 (chī)", "饭 (fàn)"],
+        correct: "吃 (chī)",
+    },
+    {
+        question: "Nhà ăn, căn tin",
+        answers: ["食堂 (shítáng)", "馒头 (mántou)"],
+        correct: "食堂 (shítáng)",
+    },
+    {
+        question: "Bánh bao chay",
+        answers: ["馒头 (mántou)", "米饭 (mǐfàn)"],
+        correct: "馒头 (mántou)",
+    },
+    {
+        question: "Gạo",
+        answers: ["米 (mǐ)", "米饭 (mǐfàn)"],
+        correct: "米 (mǐ)",
+    },
+    {
+        question: "Cần, muốn",
+        answers: ["要 (yào)", "个 (gè)"],
+        correct: "要 (yào)",
+    },
+    {
+        question: "Bát, chén",
+        answers: ["碗 (wǎn)", "鸡蛋 (jīdàn)"],
+        correct: "碗 (wǎn)",
+    },
+    {
+        question: "Trứng gà",
+        answers: ["鸡蛋 (jīdàn)", "鸡 (jī)"],
+        correct: "鸡蛋 (jīdàn)",
+    },
 ];
 
 export default level6Questions;
