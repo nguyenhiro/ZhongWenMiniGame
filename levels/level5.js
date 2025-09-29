34f6d99658bcd079007a46c4bb35800baae3073b
nguyenhiro <nguyenhirokfc@gmail.com>
2025-09-29 17:45:03 +0700

    Update level5.js
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

--- levels/level5.js
+++ levels/level5.js
@@ -1,14 +1,84 @@
 const level5Questions = [
-  {
-    question: "Câu hỏi 1 cấp độ 2",
-    answers: ["Đáp án A", "Đáp án B", "Đáp án C"],
-    correct: "Đáp án A",
-  },
-  {
-    question: "Câu hỏi 2 cấp độ 2",
-    answers: ["Đáp án X", "Đáp án Y", "Đáp án Z"],
-    correct: "Đáp án Y",
-  },
+ {
+        question: "Xin hỏi",
+        answers: ["请问 (qǐng wèn)", "问 (wèn)"],
+        correct: "请问 (qǐng wèn)",
+    },
+    {
+        question: "Họ",
+        answers: ["贵姓 (guìxìng)", "姓 (xìng)"],
+        correct: "姓 (xìng)",
+    },
+    {
+        question: "Gọi",
+        answers: ["叫 (jiào)", "名字 (míngzi)"],
+        correct: "叫 (jiào)",
+    },
+    {
+        question: "Nước, quốc gia",
+        answers: ["哪 (nǎ)", "国 (guó)"],
+        correct: "国 (guó)",
+    },
+    {
+        question: "Nước Đức",
+        answers: ["中国 (Zhōngguó)", "德国 (Déguó)"],
+        correct: "德国 (Déguó)",
+    },
+    {
+        question: "Nước Pháp",
+        answers: ["俄国 (Éguó)", "法国 (Fǎguó)"],
+        correct: "法国 (Fǎguó)",
+    },
+    {
+        question: "Giáo sư",
+        answers: ["教授 (jiàoshòu)", "校长 (xiàozhǎng)"],
+        correct: "教授 (jiàoshòu)",
+    },
+    {
+        question: "Hàn Quốc",
+        answers: ["韩国 (Hánguó)", "日本 (Rìběn)"],
+        correct: "韩国 (Hánguó)",
+    },
+    {
+        question: "Nhật Bản",
+        answers: ["日本 (Rìběn)", "英国 (Yīngguó)"],
+        correct: "日本 (Rìběn)",
+    },
+    {
+        question: "Nó",
+        answers: ["人 (rén)", "它 (tā)"],
+        correct: "它 (tā)",
+    },
+    {
+        question: "Chữ Hán",
+        answers: ["学习 (xuéxí)", "汉字 (Hànzì)"],
+        correct: "汉字 (Hànzì)",
+    },
+    {
+        question: "Cái gì, gì",
+        answers: ["什么 (shénme)", "书 (shū)"],
+        correct: "什么 (shénme)",
+    },
+    {
+        question: "Phát âm",
+        answers: ["发音 (fāyīn)", "什么 (shénme)"],
+        correct: "发音 (fāyīn)",
+    },
+    {
+        question: "Ai",
+        answers: ["谁 (shéi / shuí)", "的 (de)"],
+        correct: "谁 (shéi / shuí)",
+    },
+    {
+        question: "Tạp chí",
+        answers: ["那 (nà)", "杂志 (zázhì)"],
+        correct: "杂志 (zázhì)",
+    },
+    {
+        question: "Giáo sư",
+        answers: ["教授 (jiàoshòu)", "校长 (xiàozhǎng)"],
+        correct: "教授 (jiàoshòu)",
+    },
 ];
 
 export default level5Questions;
