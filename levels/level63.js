a168b77efc31b45dbcdeae5f2093403906f5c569
nguyenhiro <nguyenhirokfc@gmail.com>
2025-10-05 22:50:07 +0700

    Add files via upload
    Signed-off-by: nguyenhiro <nguyenhirokfc@gmail.com>

---
+++ levels/level63.js
@@ -0,0 +1,254 @@
+const level63Questions = [
+{
+    "question": "thường dân",
+    "answers": ["百姓 (bǎixìng)", "家教 (jiājiào)"],
+    "correct": "百姓 (bǎixìng)",
+  },
+  {
+    "question": "quần chúng",
+    "answers": ["老百姓 (lǎobǎixìng)", "成就 (chéngjiù)"],
+    "correct": "老百姓 (lǎobǎixìng)",
+  },
+  {
+    "question": "của tôi",
+    "answers": ["本 (běn)", "专栏 (zhuānlán)"],
+    "correct": "本 (běn)",
+  },
+  {
+    "question": "từ (bắt đầu)",
+    "answers": ["起 (qǐ)", "兼 (jiān)"],
+    "correct": "起 (qǐ)",
+  },
+  {
+    "question": "mở ra",
+    "answers": ["开办 (kāibàn)", "讲述 (jiǎngshù)"],
+    "correct": "开办 (kāibàn)",
+  },
+  {
+    "question": "chuyên mục",
+    "answers": ["专栏 (zhuānlán)", "中央 (zhōngyāng)"],
+    "correct": "专栏 (zhuānlán)",
+  },
+  {
+    "question": "trình bày",
+    "answers": ["讲述 (jiǎngshù)", "投稿 (tóu gǎo)"],
+    "correct": "讲述 (jiǎngshù)",
+  },
+  {
+    "question": "phản ánh",
+    "answers": ["反映 (fǎnyìng)", "为难 (wéinán)"],
+    "correct": "反映 (fǎnyìng)",
+  },
+  {
+    "question": "gửi bài",
+    "answers": ["投稿 (tóu gǎo)", "种地 (zhòng dì)"],
+    "correct": "投稿 (tóu gǎo)",
+  },
+  {
+    "question": "họ tên",
+    "answers": ["姓名 (xìngmíng)", "性别 (xìngbié)"],
+    "correct": "姓名 (xìngmíng)",
+  },
+  {
+    "question": "giới tính",
+    "answers": ["性别 (xìngbié)", "职业 (zhíyè)"],
+    "correct": "性别 (xìngbié)",
+  },
+  {
+    "question": "nghề nghiệp",
+    "answers": ["职业 (zhíyè)", "家务 (jiāwù)"],
+    "correct": "职业 (zhíyè)",
+  },
+  {
+    "question": "anh cả",
+    "answers": ["老大 (lǎodà)", "子 (zǐ)"],
+    "correct": "老大 (lǎodà)",
+  },
+  {
+    "question": "làm ruộng",
+    "answers": ["种地 (zhòng dì)", "照顾 (zhàogù)"],
+    "correct": "种地 (zhòng dì)",
+  },
+  {
+    "question": "lấy làm chính",
+    "answers": ["以...为主 (yǐ...wéi zhǔ)", "总之 (zǒngzhī)"],
+    "correct": "以...为主 (yǐ...wéi zhǔ)",
+  },
+  {
+    "question": "kiêm nhiệm",
+    "answers": ["兼 (jiān)", "总得 (zǒngděi)"],
+    "correct": "兼 (jiān)",
+  },
+  {
+    "question": "nghề phụ",
+    "answers": ["副业 (fùyè)", "高中 (gāozhōng)"],
+    "correct": "副业 (fùyè)",
+  },
+  {
+    "question": "cấp 3",
+    "answers": ["高中 (gāozhōng)", "初中 (chūzhōng)"],
+    "correct": "高中 (gāozhōng)",
+  },
+  {
+    "question": "đôi khi",
+    "answers": ["一时 (yīshí)", "最初 (zuìchū)"],
+    "correct": "一时 (yīshí)",
+  },
+  {
+    "question": "chăm sóc",
+    "answers": ["照顾 (zhàogù)", "伤心 (shāngxīn)"],
+    "correct": "照顾 (zhàogù)",
+  },
+  {
+    "question": "tóm lại",
+    "answers": ["总之 (zǒngzhī)", "哪怕 (nǎpà)"],
+    "correct": "总之 (zǒngzhī)",
+  },
+  {
+    "question": "cả đời",
+    "answers": ["一辈子 (yībèizi)", "安定 (āndìng)"],
+    "correct": "一辈子 (yībèizi)",
+  },
+  {
+    "question": "cần phải",
+    "answers": ["总得 (zǒngděi)", "反而 (fǎn'ér)"],
+    "correct": "总得 (zǒngděi)",
+  },
+  {
+    "question": "tự lập",
+    "answers": ["自立 (zìlì)", "为难 (wéinán)"],
+    "correct": "自立 (zìlì)",
+  },
+  {
+    "question": "biên tập viên",
+    "answers": ["编辑 (biānjí)", "歌手 (gēshǒu)"],
+    "correct": "编辑 (biānjí)",
+  },
+  {
+    "question": "đơn thân",
+    "answers": ["单亲 (dānqīn)", "孝 (xiào)"],
+    "correct": "单亲 (dānqīn)",
+  },
+  {
+    "question": "làm khó",
+    "answers": ["为难 (wéinán)", "没脸 (méiliǎn)"],
+    "correct": "为难 (wéinán)",
+  },
+  {
+    "question": "việc nhà",
+    "answers": ["家务 (jiāwù)", "成就 (chéngjiù)"],
+    "correct": "家务 (jiāwù)",
+  },
+  {
+    "question": "dù cho",
+    "answers": ["哪怕 (nǎpà)", "好在 (hǎozài)"],
+    "correct": "哪怕 (nǎpà)",
+  },
+  {
+    "question": "xảy chuyện",
+    "answers": ["出事 (chū shì)", "去世 (qùshì)"],
+    "correct": "出事 (chū shì)",
+  },
+  {
+    "question": "gia sư",
+    "answers": ["家教 (jiājiào)", "同事 (tóngshì)"],
+    "correct": "家教 (jiājiào)",
+  },
+  {
+    "question": "cấp 2",
+    "answers": ["初中 (chūzhōng)", "高中 (gāozhōng)"],
+    "correct": "初中 (chūzhōng)",
+  },
+  {
+    "question": "may mà",
+    "answers": ["好在 (hǎozài)", "总得 (zǒngděi)"],
+    "correct": "好在 (hǎozài)",
+  },
+  {
+    "question": "vâng lời",
+    "answers": ["听话 (tīnghuà)", "懂事 (dǒngshì)"],
+    "correct": "听话 (tīnghuà)",
+  },
+  {
+    "question": "bên trong",
+    "answers": ["内 (nèi)", "身边 (shēnbiān)"],
+    "correct": "内 (nèi)",
+  },
+  {
+    "question": "biết điều",
+    "answers": ["懂事 (dǒngshì)", "安定 (āndìng)"],
+    "correct": "懂事 (dǒngshì)",
+  },
+  {
+    "question": "ca sĩ",
+    "answers": ["歌手 (gēshǒu)", "编辑 (biānjí)"],
+    "correct": "歌手 (gēshǒu)",
+  },
+  {
+    "question": "ban đầu",
+    "answers": ["最初 (zuìchū)", "一时 (yīshí)"],
+    "correct": "最初 (zuìchū)",
+  },
+  {
+    "question": "Trung ương",
+    "answers": ["中央 (zhōngyāng)", "内 (nèi)"],
+    "correct": "中央 (zhōngyāng)",
+  },
+  {
+    "question": "qua đời",
+    "answers": ["去世 (qùshì)", "出事 (chū shì)"],
+    "correct": "去世 (qùshì)",
+  },
+  {
+    "question": "ổn định",
+    "answers": ["安定 (āndìng)", "伤心 (shāngxīn)"],
+    "correct": "安定 (āndìng)",
+  },
+  {
+    "question": "ngược lại",
+    "answers": ["反而 (fǎn'ér)", "好在 (hǎozài)"],
+    "correct": "反而 (fǎn'ér)",
+  },
+  {
+    "question": "đau lòng",
+    "answers": ["伤心 (shāngxīn)", "懂事 (dǒngshì)"],
+    "correct": "伤心 (shāngxīn)",
+  },
+  {
+    "question": "bên cạnh",
+    "answers": ["身边 (shēnbiān)", "中央 (zhōngyāng)"],
+    "correct": "身边 (shēnbiān)",
+  },
+  {
+    "question": "thành tựu",
+    "answers": ["成就 (chéngjiù)", "职业 (zhíyè)"],
+    "correct": "成就 (chéngjiù)",
+  },
+  {
+    "question": "mất mặt",
+    "answers": ["没脸 (méiliǎn)", "为难 (wéinán)"],
+    "correct": "没脸 (méiliǎn)",
+  },
+  {
+    "question": "đồng nghiệp",
+    "answers": ["同事 (tóngshì)", "老大 (lǎodà)"],
+    "correct": "同事 (tóngshì)",
+  },
+  {
+    "question": "hiếu thảo",
+    "answers": ["孝 (xiào)", "听话 (tīnghuà)"],
+    "correct": "孝 (xiào)",
+  },
+  {
+    "question": "trợ từ",
+    "answers": ["之 (zhī)", "本 (běn)"],
+    "correct": "之 (zhī)",
+  },
+  {
+    "question": "con trai",
+    "answers": ["子 (zǐ)", "百姓 (bǎixìng)"],
+    "correct": "子 (zǐ)"
+  }
+]
+
+export default level63Questions;
